d9d5d365b3a871a8ee2f28665ed6c3078ad296f7
mcastillo40 <chgreen08@gmail.com>
2020-12-17 11:45:37 -0800

    Add Cors Policy

--- Reactivities/API/Startup.cs
+++ Reactivities/API/Startup.cs
@@ -23,6 +23,15 @@ namespace API
             services.AddDbContext<DataContext>(opt =>{
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+
             services.AddControllers();
         }
 
@@ -39,6 +48,8 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
