e54e626fc6ea89f0f6c7ac46589da4d14f9df49f
mcastillo40 <chgreen08@gmail.com>
2021-01-21 14:17:56 -0800

    - Implement simple CRUD actions for clent-app  - Implement basic style of application

---
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Grid } from 'semantic-ui-react'
+import { IActivity } from '../../../app/models/activity'
+import { ActivitiyDetails } from '../details/ActivitiyDetails'
+import { ActivityForm } from '../form/ActivityForm'
+import { ActivityList } from './ActivityList'
+
+interface IProps {
+    activities: IActivity[];
+    selectActivity: (id: string) => void;
+    selectedActivity: IActivity | null;
+    editMode: boolean;
+    setEditMode : (editMode : boolean) => void;
+    setSelectedActivity : (activity: IActivity | null) => void
+    createActivity : (activity: IActivity) => void
+    editActivity : (activity: IActivity) => void
+    deleteActivity : (id: string) => void
+}
+
+export const ActivityDashboard: React.FC<IProps> = ({
+        activities, 
+        selectActivity, 
+        selectedActivity, 
+        editMode, 
+        setEditMode,
+        setSelectedActivity,
+        createActivity,
+        editActivity,
+        deleteActivity
+    }) => {
+    return (
+        <Grid>
+            <Grid.Column width={10}>
+                <ActivityList 
+                    activities={activities} 
+                    selectActivity={selectActivity} 
+                    deleteActivity={deleteActivity}
+                />
+            </Grid.Column>
+            <Grid.Column width={6}>
+                { selectedActivity && !editMode && 
+                <ActivitiyDetails 
+                    activity={selectedActivity} 
+                    setSelectedActivity={setSelectedActivity} 
+                    setEditMode={setEditMode}/> }
+                { editMode && 
+                    <ActivityForm 
+                        key={(selectedActivity && selectedActivity.id) || 0}
+                        activity={selectedActivity!} 
+                        setEditMode={setEditMode}
+                        createActivity={createActivity}
+                        editActivity={editActivity}
+                    /> 
+                }
+            </Grid.Column>
+        </Grid>
+    )
+}
